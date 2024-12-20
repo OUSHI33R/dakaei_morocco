917e10452ae335afe3b1bd6cb506a8a79e53e4a3
OUSHI33R <ouchi33r@gmail.com>
2024-12-21 00:23:16 +0100

    Logo Ma

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -11,23 +11,17 @@ export function Logo() {
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900">
+    <div className="flex items-center justify-center h-screen bg-black">
       <div
-        className={`
-          relative h-48 w-48 sm:h-64 sm:w-64 lg:h-80 lg:w-80
-          transform transition-all duration-1000 ease-out
-          ${
-            isLoaded
-              ? "translate-y-0 opacity-100 scale-100"
-              : "translate-y-10 opacity-0 scale-90"
-          }
-        `}
+        className={`relative h-40 w-40 sm:h-56 sm:w-56 lg:h-64 lg:w-64 transform transition-all duration-700 ease-out ${
+          isLoaded ? "opacity-100 scale-100" : "opacity-0 scale-95"
+        }`}
       >
         <Image
           src="https://i.ibb.co/R3cMPnR/Ma-Dakaei-Byed.png"
           alt="Logo"
           fill
-          className="object-contain rounded-full shadow-lg shadow-gray-900/50"
+          className="object-contain rounded-md shadow-xl"
           priority
           onLoad={() => setIsLoaded(true)}
         />
