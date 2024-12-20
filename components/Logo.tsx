cbc1e8ae1862124bef85f83d4c80d433468d53f7
OUSHI33R <ouchi33r@gmail.com>
2024-12-21 00:17:16 +0100

    Add Logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
+import { useState, useEffect } from "react";
+import Image from "next/image";
 
 export function Logo() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -12,15 +12,15 @@ export function Logo() {
 
   return (
     <div className="text-center">
-      <div 
+      <div
         className={`
           relative h-48 w-48 md:h-64 md:w-64 
           transform transition-all duration-1000 ease-out
-          ${isLoaded ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'}
+          ${isLoaded ? "translate-y-0 opacity-100" : "translate-y-10 opacity-0"}
         `}
       >
         <Image
-          src="https://i.ibb.co/XyJkn61/searchh.png"
+          src="https://i.ibb.co/R3cMPnR/Ma-Dakaei-Byed.png"
           alt="Logo"
           fill
           className="object-contain"
