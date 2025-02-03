9118cc13c49259e98eecbc7da34bc1837c0d3f9e
OUSHI33R <ouchi33r@gmail.com>
2025-02-03 14:32:47 +0100

    Opengraphe

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,20 @@ export const metadata: Metadata = {
     icon: "https://i.ibb.co/XyJkn61/searchh.png",
     shortcut: "https://i.ibb.co/XyJkn61/searchh.png",
   },
+  openGraph: {
+    title: "DAKAEi Morocco",
+    description: "First Moroccan AI Platform",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/images/c41dc69f-d6e7-429b-b8bf-4bec868c4d18.jpg?token=LDKVEP-7h3zkX_lQIDAb0C4t4t-gCLb-2KZB0FQHob8&height=675&width=1200&expires=33274510245",
+        width: 1200,
+        height: 675,
+        alt: "DAKAEi Morocco OpenGraph Image",
+      },
+    ],
+    type: "website",
+    url: "https://dakaei.ma", // Replace with your actual website URL
+  },
 };
 
 const tajawal = Tajawal({
