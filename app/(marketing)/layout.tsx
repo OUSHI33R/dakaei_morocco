477b33c7c33b21fb701f2760fe1b8e632235a6ca
OUSHI33R <ouchi33r@gmail.com>
2025-04-08 20:11:33 +0100

    Updates Ligal Layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,7 @@
-import { Logo } from "@/components/ui/logo";
+
 import "./globals.css";
-import { Tajawal } from "next/font/google";
-import Link from "next/link";
 import type React from "react";
-import { Button } from "@/components/ui/button";
-import { Download } from "lucide-react";
-import { PiGooglePlayLogoFill } from "react-icons/pi";
+
 import Header from "@/components/Header";
 
 export default function MarketingLayout({
